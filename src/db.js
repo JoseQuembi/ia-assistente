c77c1694580aa485ffabd7f31f932444fe6ce44e
JoseQuembi <josequembi@gmail.com>
2024-06-09 17:34:50 +0100

    Atualizando as configurações do banco de dados

--- src/db.js
+++ src/db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'quembi',
-    database: process.env.DB_NAME || 'quembi'
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || ''
 });
 
 connection.connect(err => {
